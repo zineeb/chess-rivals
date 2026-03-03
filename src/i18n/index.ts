6e8bc4f91c87808576e4bd936888fe96ef9bbadb
zouzou <mouazzazz@gmail.com>
2026-03-03 01:40:00 +0100

    feat: add i18n configuration with language detection

---
+++ src/i18n/index.ts
@@ -0,0 +1,26 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import enTranslation from './locales/en/translation.json';
+import frTranslation from './locales/fr/translation.json';
+
+i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources: {
+            en: { translation: enTranslation },
+            fr: { translation: frTranslation },
+        },
+        fallbackLng: 'en',
+        supportedLngs: ['en', 'fr'],
+        interpolation: {
+            escapeValue: false,
+        },
+        detection: {
+            order: ['localStorage', 'navigator'],
+        },
+    });
+
+export default i18n;
